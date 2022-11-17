1d4061c294a4167395db124248d95cea2621ecbb
andrewmalley725 <andrew.alley99@gmail.com>
2022-11-17 16:13:50 -0700

    security

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const knex = require('knex')({
       host : '127.0.0.1',
       port : 3306,
       user : 'root',
-      password : 'Andyman72599',
+      password : '********',
       database : 'helpdesk'
     }
 });
